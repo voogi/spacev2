3311b4bb5f9d98bfaf6374fa485ee278b053d7bc
Völgyi Dániel <lofutty23xx@gmail.com>
2017-06-18 16:43:35 +0200

    lemaradt egy queue

---
+++ src/app/services/progress.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {Subject} from "rxjs/Subject";
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+
+@Injectable()
+export class ProgressService {
+
+  private progressSubject: Subject<any> = new Subject();
+  private completedProgressSubject: Subject<any> = new Subject();
+
+  constructor() {}
+
+  createProgress(obj:any){
+    this.progressSubject.next(obj)
+  }
+
+  addedProgress() : Observable<any>{
+    return this.progressSubject;
+  }
+
+  onCompletedProgress(data:any){
+    this.completedProgressSubject.next(data);
+  }
+
+  onComplete() : Observable<any>{
+    return this.completedProgressSubject;
+  }
+
+}
